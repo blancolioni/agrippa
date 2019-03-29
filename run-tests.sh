1454bc98162542814c0b28baf80480ffad2db52a
Fraser Wilson <blancolioni@gmail.com>
2019-03-29 08:52:44 +0100

    run-tests: run 100 test games

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 mkdir -p test-results
-for seed in 1 2 3 4 5 6 7 8 9
+for x in 0 1 2 3 4 5 6 7 8 9
 do
-   ./build/bin/agrippa-driver --random-seed=$seed > test-results/test-$seed.log 2> test-results/error-$seed.log
+  for y in 0 1 2 3 4 5 6 7 8 9
+  do
+    ./build/bin/agrippa-driver --random-seed=$x$y > test-results/test-$x$y.log 2> test-results/error-$x$y.log
+  done
 done
