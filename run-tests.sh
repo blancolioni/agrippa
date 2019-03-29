1de36bbd1f757793a61dde4d7aa159436d476f73
Fraser Wilson <blancolioni@gmail.com>
2019-03-29 14:40:13 +0100

    run-tests.sh: small refactor.  Remove empty error files.

--- run-tests.sh
+++ run-tests.sh
@@ -4,6 +4,13 @@ for x in 0 1 2 3 4 5 6 7 8 9
 do
   for y in 0 1 2 3 4 5 6 7 8 9
   do
-    ./build/bin/agrippa-driver --random-seed=$x$y > test-results/test-$x$y.log 2> test-results/error-$x$y.log
+    seed=$x$y
+    output=test-results/test-$seed.log
+    error=test-results/error-$seed.log
+    ./build/bin/agrippa-driver --random-seed=$seed > $output 2> $error
+    if [ $(stat -c %s $error) -eq 0 ]
+    then rm $error
+    else echo Test $seed failed
+    fi
   done
 done
