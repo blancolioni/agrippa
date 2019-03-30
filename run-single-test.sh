6eb6ebc4bb9e2bdb4420901baf10a3888d94c9b6
Fraser Wilson <blancolioni@gmail.com>
2019-03-30 16:26:24 +0100

    New shell script: run-single-test.sh

---
+++ run-single-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p test-results
+seed=$1
+output=test-results/test-$seed.log
+error=test-results/error-$seed.log
+./build/bin/agrippa-driver --random-seed=$seed > $output 2> $error
+if [ $(stat -c %s $error) -eq 0 ]
+then rm $error
+else echo Test $seed failed
+fi
