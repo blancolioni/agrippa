857c44baafafb0783eac6c71396feb13e704c183
Fraser Wilson <fraser@thewhitelion.eu>
2021-11-03 10:14:37 +0100

    run-single-test: added --text-ui option

--- run-single-test.sh
+++ run-single-test.sh
@@ -3,7 +3,7 @@ mkdir -p test-results
 seed=$1
 output=test-results/test-$seed.log
 error=test-results/error-$seed.log
-./build/bin/agrippa-driver --random-seed=$seed > $output 2> $error
+./build/bin/agrippa-driver --text-ui --random-seed=$seed > $output 2> $error
 if [ $(stat -c %s $error) -eq 0 ]
 then rm $error
 else echo Test $seed failed
